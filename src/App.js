a99045549c93504c137aa39629bf1602964fb7a2
Arnulfo Antonio Miramontes Beas <arnulfo_amb182@hotmail.com>
2021-01-17 18:14:58 -0600

    fix - sign in with google button causes the email and password fields to trigger asking to fill these in

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  constructor(props) {
+  constructor() {
     super();
 
     this.state = {
