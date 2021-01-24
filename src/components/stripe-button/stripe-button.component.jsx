1c4aee94f78ddae2f9c603d4becdfe6bfc05efa0
Arnulfo Antonio Miramontes Beas <arnulfo_amb182@hotmail.com>
2021-01-24 13:10:03 -0600

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51IDCcrJC9j2oSutLAs7JPDdYHR24Cjb2rMRS52Nu4jGKsCSKHbK4d8y6ICFt6DKvRLEdkrTIgn85zRd8vnRZelfe00OzC5R7Sc'
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
